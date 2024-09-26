40d862b6dd4f9bd6694916452cb7cc03cecc2034
TrustSign123 <developer.trustsign@gmail.com>
2024-09-26 17:24:12 +0530

    fix 2 v0.2.0

---
+++ src/lib/utils/authOptions.ts
@@ -0,0 +1,65 @@
+import NextAuth, { User } from "next-auth";
+import { AuthOptions } from "next-auth";
+import { compare } from "bcrypt";
+import { connectToDB } from "@/lib/mongoDB/mongoose";
+import DevUser from "@/lib/models/DevUser";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
+import FacebookProvider from "next-auth/providers/facebook";
+import TwitchProvider from "next-auth/providers/twitch";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+    providers: [
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+      DiscordProvider({
+        clientId: process.env.DISCORD_CLIENT_ID as string,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      }),
+      FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID as string,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+      }),
+      TwitchProvider({
+        clientId: process.env.TWITCH_CLIENT_ID as string,
+        clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
+      }),
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: { label: "Email", type: "email" },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(
+          credentials: Record<"email" | "password", string> | undefined
+        ): Promise<User | null> {
+          if (!credentials) {
+            throw new Error("Credentials not provided");
+          }
+          await connectToDB();
+  
+          const user = await DevUser.findOne({
+            email: credentials?.email,
+          });
+  
+          if (user && (await compare(credentials?.password, user.password))) {
+            return {
+              id: user._id.toString(),
+              email: user.email,
+              name: user.name,
+            };
+          }
+  
+          return null;
+        },
+      }),
+    ],
+  };
